4bd0a247ac713fd78521dab20156b41bc5d73280
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-08-11 06:32:36 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,54 @@
+function add() {
+    
+}
+ 
+function subtract() {
+    
+}
+
+function multiply() {
+    
+}
+
+function divide() {
+    
+}
+
+function add(a, b) {
+    return(a+b)
+}
+
+function subtract(a, b) {
+    return(a-b)
+}
+
+function multiply(a, b) {
+    return(a*b)
+}
+
+function divide(a, b) {
+    return(a/b)
+}
+
+function  increment(n) {
+    return(++n)
+}
+
+function  decrement(n) {
+    return(--n)
+}
+function parseInt(n) {
+    return (n)
+}
+
+function makeInt(n) {
+    return parseInt(n)
+}
+
+function makeInt(string) {
+    return parseFloat(string, 10)
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string)
+}
